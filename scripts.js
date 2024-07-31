4ebfc4a0da533957bc1ff40a241b1b96bc90d8c7
Mizuba74 <kevinleal@hotmail.com>
2024-07-31 14:46:40 +0200

    Creation of funciton that creates columns, it works

--- scripts.js
+++ scripts.js
@@ -1,8 +1,44 @@
-//Creation of a x*y grid of divs
+
 
 const container = document.querySelector("#container");
-const numberSquaresHeight = document.createElement("div");
-const numberSquaresWidth = document.createElement("div");
 
 
 
+
+
+
+//Creation of a x*y grid of divs
+function gridCreation(numberSquares){
+
+    for(let i = 0; i < numberSquares; i++){
+
+        const numberColumn = document.createElement("div");
+        numberColumn.className = ("column");
+
+        const numberLines = document.createElement("div");
+        numberLines.className = ("lines");
+
+        document.querySelector("#container").appendChild(numberColumn);
+        numberColumn.appendChild(numberLines);
+
+    }
+}
+
+
+
+
+
+
+
+ // let autoScale = (480/numberOfSquares);
+
+    // for(let i = 0; i < numberOfSquares; i++){
+    //     const numberSquares = document.createElement("div");
+    //     numberSquares.className = ("squares");
+
+    //     numberSquares.style.width = (autoScale + "px");
+    //     numberSquares.style.height = (autoScale + "px");
+
+
+    //     document.querySelector("#container").appendChild(numberSquares);
+    // }
