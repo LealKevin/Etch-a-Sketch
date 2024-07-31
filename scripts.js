e0a005a047ef28d3b30002dcc5a03851acc7ea3d
Mizuba74 <kevinleal@hotmail.com>
2024-07-31 12:46:13 +0200

    Tested the container with divs in html, seems working

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//Creation of a x*y grid of divs
+
+const container = document.querySelector("#container");
+const numberSquaresHeight = document.createElement("div");
+const numberSquaresWidth = document.createElement("div");
+
+
+
